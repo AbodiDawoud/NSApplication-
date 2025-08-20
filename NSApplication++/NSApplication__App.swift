021aaf58ba26979050c7783b9dff3ecdb462b615
AbodiDawood <mo7.daawoud@icloud.com>
2025-08-20 02:07:18 +0200

    Initial Commit

---
+++ NSApplication++/NSApplication__App.swift
@@ -0,0 +1,15 @@
+//
+//  NSApplication__App.swift
+//  NSApplication++
+    
+
+import SwiftUI
+
+@main
+struct NSApplication__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
